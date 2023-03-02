3f590bdba7fb061cba13ea6f1599e92dc4d8d458
unknown <shuvrodevmondal337@gmail.com>
2023-03-03 05:35:08 +0600

    added and designed details button

--- js/ai.js
+++ js/ai.js
@@ -14,16 +14,19 @@ tools.forEach(tool => {
     toolCardDiv.classList.add("col")
     toolCardDiv.innerHTML=`
     <div class="card h-100" >
-                <img src="${image}" class="card-img-top" alt="">
+                <img src="${image}" class="card-img-top img-responsive" alt="">
                 <div class="card-body">
-                  <h5 class="card-title">Features</h5>
-                  <div> ${showFeatures(features)} </div>
+                    <h5 class="card-title">Features</h5>
+                    <div> ${showFeatures(features)} </div>
                 </div>
-                <div class="card-footer">
-                 <h5 class="card-title">${name}</h5>
-
-                 <p class="card-text">1. ${published_in}</p>
-                  
+                <div class="card-footer d-flex justify-content-between align-items-center">
+                     <div>
+                      <h5 class="card-title">${name}</h5>
+                      <p class="card-text">${published_in}</p>
+                    </div>
+                    <div>
+                      <button type="button" class="btn btn-outline-danger"><i class="fa-solid fa-2xl fa-arrow-right"></i></button>
+                    </div>
                 </div>
     </div>
     
@@ -40,23 +43,19 @@ toolsContainer.appendChild(toolCardDiv)
 
 
 
+// Handle features of tool card 
 const showFeatures=(features)=> {
     let count=0;
     const featureDiv=document.createElement("div")
-    featureDiv.style.listStyleType="none"
     for (const feature of features) {
         count++
         const p=document.createElement("p")
         p.innerText=`${count}. ${feature}`
         featureDiv.appendChild(p)
     }
-    console.log(featureDiv);
     return featureDiv.innerHTML
 }
 
 
 
 
-{/* <p class="card-text">1. ${features[0]}</p>
-                  <p class="card-text">2. ${features[1]}</p>
-                  <p class="card-text">3. ${features[2]}</p> */}
