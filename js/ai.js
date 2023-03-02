4f42f614ef65899df6e5e40e73c70026e0b5a666
unknown <shuvrodevmondal337@gmail.com>
2023-03-03 04:56:34 +0600

    made features list dynamic

--- js/ai.js
+++ js/ai.js
@@ -10,20 +10,14 @@ const showAITools=(tools)=>{
 const toolsContainer=document.getElementById("tool-cards")
 tools.forEach(tool => {
     const {image,features,name,published_in}=tool
-    // console.log(image);
-    // console.log(features);
-    // console.log(name);
-    // console.log(published_in);
     const toolCardDiv=document.createElement("div")
     toolCardDiv.classList.add("col")
     toolCardDiv.innerHTML=`
-    <div class="card h-100">
+    <div class="card h-100" >
                 <img src="${image}" class="card-img-top" alt="">
                 <div class="card-body">
                   <h5 class="card-title">Features</h5>
-                  <p class="card-text">1. ${features[0]}</p>
-                  <p class="card-text">2. ${features[1]}</p>
-                  <p class="card-text">3. ${features[2]}</p>
+                  <div> ${showFeatures(features)} </div>
                 </div>
                 <div class="card-footer">
                  <h5 class="card-title">${name}</h5>
@@ -35,5 +29,34 @@ tools.forEach(tool => {
     
     `
 toolsContainer.appendChild(toolCardDiv)
+
+
 });
+
+
 }
+
+
+
+
+
+const showFeatures=(features)=> {
+    let count=0;
+    const featureDiv=document.createElement("div")
+    featureDiv.style.listStyleType="none"
+    for (const feature of features) {
+        count++
+        const p=document.createElement("p")
+        p.innerText=`${count}. ${feature}`
+        featureDiv.appendChild(p)
+    }
+    console.log(featureDiv);
+    return featureDiv.innerHTML
+}
+
+
+
+
+{/* <p class="card-text">1. ${features[0]}</p>
+                  <p class="card-text">2. ${features[1]}</p>
+                  <p class="card-text">3. ${features[2]}</p> */}
